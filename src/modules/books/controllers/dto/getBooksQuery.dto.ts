b555bc192c5d3f76124e21da9c3ed617f85e4ca9
Tal Kohavy <talkohavy@gmail.com>
2026-02-12 22:18:12 +0200

    unused category

--- src/modules/books/controllers/dto/getBooksQuery.dto.ts
+++ src/modules/books/controllers/dto/getBooksQuery.dto.ts
@@ -3,4 +3,7 @@ import Joi from 'joi';
 export const getBooksQuerySchema = Joi.object({
   page: Joi.number().integer().min(1).default(1),
   limit: Joi.number().integer().min(1).max(100).default(20),
+  // sortBy: Joi.string().optional(),
+  // sortDirection: Joi.string().optional(),
+  category: Joi.string().optional(), // <---currently not being used
 });
