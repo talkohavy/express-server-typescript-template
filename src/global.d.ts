8a38b63a17400e1159315379877440849fbdc4d6
Tal Kohavy <talkohavy@gmail.com>
2026-02-12 09:26:58 +0200

    joiQueryMiddleware: added castedValues to queryParsed

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,11 @@ import type { OptimizedApp } from './common/types';
 declare module 'express' {
   export interface Request {
     user?: Record<string, any>;
+    query: any;
+    /**
+     * Only exists after applying the `joiQueryMiddleware`.
+     */
+    queryParsed?: any;
   }
 
   export interface Application extends OptimizedApp {
--- src/middlewares/joi-query.middleware.ts
+++ src/middlewares/joi-query.middleware.ts
@@ -6,12 +6,11 @@ export function joiQueryMiddleware(validationSchema: Joi.ObjectSchema<any>): any
   return function validateUsingJoi(req: Request, _res: Response, next: NextFunction) {
     const { query } = req;
 
-    const { error } = validationSchema.validate(query);
+    const { error, value: castedValues } = validationSchema.validate(query);
 
-    if (error) {
-      console.log('Validation error', error);
-      throw new BadRequestError(error.message);
-    }
+    req.queryParsed = castedValues;
+
+    if (error) throw new BadRequestError(error.message);
 
     next();
   };
