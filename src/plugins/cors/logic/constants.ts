d74425298284bd159e7f48a9e574016c5e9cfc04
Tal Kohavy <talkohavy@gmail.com>
2026-03-13 15:58:26 +0200

    added 3005 to cors

--- src/plugins/cors/logic/constants.ts
+++ src/plugins/cors/logic/constants.ts
@@ -1,6 +1,7 @@
 export const ALLOWED_DOMAINS: Array<string> = [
   'http://localhost:3000',
   'http://localhost:3004',
+  'http://localhost:3005',
   'https://luckylove.co.il',
 ];
 export const DOMAIN_REGEX = '.luckylove.co.il';
