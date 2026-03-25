1ed01f109ba9a461597e47c5eb5e5c1fa9e67327
Tal Kohavy <talkohavy@gmail.com>
2026-03-25 06:49:00 +0200

    remove mock app

--- src/mockApp.ts
+++
@@ -1,8 +0,0 @@
-import { buildApp } from './buildApp';
-import type { Application } from 'express';
-
-export async function buildMockApp(app: Application) {
-  await buildApp(app);
-
-  return app;
-}
