f9b9417628bd5b311b3258985ca4fb5bf3ad4e9d
Tal Kohavy <talkohavy@gmail.com>
2026-05-16 11:16:17 +0300

    deleted websocket-manager

--- src/lib/websocket-manager/index.ts
+++
@@ -1,8 +0,0 @@
-export { WebsocketManager } from './websocket-manager';
-
-// constants
-export { TopicManager } from './logic/topic-manager';
-export { BUILT_IN_WEBSOCKET_EVENTS, WS_TOPIC_PUBSUB_CHANNEL } from './logic/constants';
-
-// types
-export type { ServerSocketResponse, TopicPayload } from './types';
